3eba82be87afa1233cadd4849cec830ff36b00c8
HongxiZhang914 <60370574+HongxiZhang914@users.noreply.github.com>
2020-02-01 17:12:18 -0500

    Add files via upload

---
+++ Intuo/app/src/main/java/com/example/intuo/Date.java
@@ -0,0 +1,43 @@
+package com.example.intuo;
+import java.time.LocalDateTime;
+public class Date {
+    private Time time;
+    private int day;
+    private int month;
+    private int year;
+    LocalDateTime local = LocalDateTime.now();              //current local date and time
+    public Date(Time t, int d, int m, int y){
+        this.time = t;
+        this.day = d;
+        this.month = m;
+        this.year = y;
+    }
+    //check if the year is a leap year
+    public boolean isLeapYear(){
+        if(year % 400 == 0 || (year % 4 == 0 && year % 100 !=0)){
+            return true;
+        }
+        return false;
+    }
+    //check if the date is equal to the local date
+    //@return true: if year, month and day all equal to local date
+    public boolean isEqualToLocalDate(){
+        return (this.year == local.getYear()&& this.month == local.getMonthValue()&& this.day == local.getDayOfMonth());
+    }
+    //find the last day of the month, this will be used in other classes to prevent invalid date, such as Feb 30...
+    //@return the last day of the month
+    public int lastDayOfMonth(){
+        switch(this.month){
+            case 2:
+                if (isLeapYear())  return 29;
+                else return 28;
+            case 4: case 6: case 9: case 11: return 30;
+            case 1: case 3: case 5: case 7: case 8: case 10: case 12: return 31;
+        }
+        return 0;
+    }
+    //debugging used method
+    public void print(){
+        System.out.println("current date is: "+year+"/"+month+"/"+day);
+    }
+}
---
+++ Intuo/app/src/main/java/com/example/intuo/Time.java
@@ -0,0 +1,25 @@
+package com.example.intuo;
+
+public class Time {
+    private int second;
+    private int minute;
+    private int hour;
+    //constructor
+    public Time(int s, int m, int h){
+        this.second = s;
+        this.minute = m;
+        this.hour = h;
+    }
+    //convert time to second in order to compare the time
+    public int convertToSecond(){
+        return hour*3600+minute*60+second;
+    }
+    //compare two time, return true if they are equal
+    public boolean isEqual(Time t){
+        return (convertToSecond() == t.convertToSecond());
+    }
+    //debugging used method
+    public void print(){
+        System.out.println("Current time is: "+ hour +":"+minute+""+minute);
+    }
+}
