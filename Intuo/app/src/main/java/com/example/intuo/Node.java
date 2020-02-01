3fca9cc2f71915bd50eaa3fe09be2ecd5fb4d212
wengehuihuang <60371399+wengehuihuang@users.noreply.github.com>
2020-02-01 18:53:56 -0500

    Create Node.java

---
+++ Intuo/app/src/main/java/com/example/intuo/Node.java
@@ -0,0 +1,13 @@
+package com.example.intuo;
+public class Node {
+
+    public Event event;
+    public Node next = null;
+
+    public Node(Event event) {
+        this.event = event;
+    }
+    public Node(){};
+
+
+}
