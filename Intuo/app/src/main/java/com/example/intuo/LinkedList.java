8a662707e6b926d3fec53b249214890dad5b0e39
wengehuihuang <60371399+wengehuihuang@users.noreply.github.com>
2020-02-01 18:47:50 -0500

    Add files via upload

---
+++ Intuo/app/src/main/java/com/example/intuo/LinkedList.java
@@ -0,0 +1,41 @@
+public class LinkedList {
+    // 没有Node呢还
+    private Node head;
+
+    public LinkedList() {
+        this.head = null;
+        
+    }
+    //insert First Node
+    public void insertHeadNode(Event event) {
+        Node node = new Node(event);
+        node.next = head;
+        head = node;
+    }
+
+    //delete Head Node
+    public Event deleteHeadNode() {
+        if (head == null) {
+            System.out.println("Empty List! ");
+            return null;
+        }
+        Event event = head.event;
+        head = head.next;
+        return event;
+    }
+
+
+
+
+    public void printLinkedList() {
+        System.out.println("Linked List");
+        if (head == null) {
+            System.out.println("Empty List! ");
+        }
+        Node current = head;
+        while (current != null) {
+            System.out.println(current);
+            current = current.next;
+        }
+    }
+}
