b3d4fc9b8d00096ab0f878135733b520dd13f32d
wengehuihuang <60371399+wengehuihuang@users.noreply.github.com>
2020-02-01 19:15:12 -0500

    Update LinkedList.java

--- Intuo/app/src/main/java/com/example/intuo/LinkedList.java
+++ Intuo/app/src/main/java/com/example/intuo/LinkedList.java
@@ -5,7 +5,7 @@ public class LinkedList {
 
     public LinkedList() {
         this.head = null;
-        
+
     }
     //insert First Node
     public void insertHeadNode(Event event) {
@@ -25,18 +25,71 @@ public class LinkedList {
         return event;
     }
 
+    // insert in anywhere
+    public void insert( Event event, int index){
 
-
-
-    public void print() {
-        System.out.println("Linked List");
         if (head == null) {
             System.out.println("Empty List! ");
+            return;
         }
+        if (index == 0) {
+            insertHeadNode(event); //insert First Node
+            return;
+        }
+
+        Node node = new Node(event);
         Node current = head;
-        while (current != null) {
-            System.out.println(current);
+
+        for (int j = 0; j < (index - 1) && current != null; j++) {
             current = current.next;
+            if (current == null) {
+                System.out.println("No such node! ");
+                return;
+            }
+            node.next = current.next;
+            current.next = node;
         }
+    }
+            //delete in anywhere
+    public Event delete(int index) {
+        if (head == null) {
+            System.out.println("Empty list! ");
+            return null;
+        }
+        if (index == 0) {
+            return deleteHeadNode();//delete Head Node
+        }
+
+        Node current = head;
+        Node remove;
+        for (int j = 0; j < (index - 1) && current != null; j++) {
+            current = current.next;
+            if (current == null) {
+                System.out.println("No such node! ");
+                return null;
+            }
+        }
+        remove = current.next;
+        Event event = remove.event;
+        current.next = remove.next;
+        return event;
+    }
+
+            //get size of list
+    public int getSize(){
+                return size;
+    }
+
+            //print out list
+    public void print() {
+                System.out.println("Linked List");
+                if (head == null) {
+                    System.out.println("Empty List! ");
+                }
+                Node current = head;
+                while (current != null) {
+                    System.out.println(current);
+                    current = current.next;
+                }
     }
 }
