6c5b6cb5780fe167098c209f5bd3680ebb55f4f1
EvelynYihuiYang <30358116+EvelynYihuiYang@users.noreply.github.com>
2020-02-02 15:46:29 -0500

    Add files via upload

---
+++ Intuo/app/src/main/java/com/example/intuo/Budget.java
@@ -0,0 +1,40 @@
+package com.example.intuo;
+
+public class Budget {
+    private String name;
+    private static int amount;
+    private int usage;
+    private Date startDate;
+    private Date endDate;
+    private int priority;
+
+    public Budget(String n, int a, int u, Date s, Date e, int p){
+        this.name = n;
+        this.amount = a;
+        this.usage = u;
+        this.startDate = s;
+        this. endDate = e;
+        this.priority = p;
+    }
+
+    // Check if this priority is greater than parameter
+    public boolean priorityCompare(int priority) {
+        return this.priority >= priority;
+    }
+
+    public void thirtyPercentWarning(){
+        if (usage >= amount * 0.7) {
+            System.out.println("30% percent left");
+        }
+    }
+
+    public void tenPercentWarning(){
+        if (usage >= amount * 0.9) {
+            System.out.println("10% percent left");
+        }
+    }
+
+    public void print(){
+        System.out.println("Budget is: name: "+ name + "amount: " + amount + "usage: " + usage + "startDate: " + startDate + "endDate: " + endDate + "priority: " + priority);
+    }
+}
---
+++ Intuo/app/src/main/java/com/example/intuo/Event.java
@@ -0,0 +1,22 @@
+package com.example.intuo;
+
+public class Event {
+    private Date date;
+    private String name;
+    private int priority;
+
+    public Event(Date d, String n, int p){
+        this.date = d;
+        this.name = n;
+        this.priority = p;
+    }
+
+    // Check if this priority is greater than parameter
+    public boolean priorityCompare(int priority) {
+        return this.priority >= priority;
+    }
+
+    public void print(){
+        System.out.println("Event is: Date: "+ date + "name: " + name + "priority: " + priority);
+    }
+}
