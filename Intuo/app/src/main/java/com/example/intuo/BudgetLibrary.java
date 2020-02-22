7c5543c0c865d1fa60fc5196e0919d9ce88b79da
wengehuihuang <60371399+wengehuihuang@users.noreply.github.com>
2020-02-22 14:49:51 -0500

    Update BudgetLibrary.java

--- Intuo/app/src/main/java/com/example/intuo/BudgetLibrary.java
+++ Intuo/app/src/main/java/com/example/intuo/BudgetLibrary.java
@@ -1,1 +1,15 @@
 
+package com.example.intuo;
+
+public class BudgetLibrary {
+
+  private BudgetList listB
+  
+  public void addEvent(Event e){
+    
+  }
+  public void print(){
+    System.out.println("Budget Library: ")
+      
+  }
+}
