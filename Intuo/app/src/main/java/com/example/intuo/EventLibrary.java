2b423f8d182fc5f02bd20d56ca7b8e3deb0c864b
wengehuihuang <60371399+wengehuihuang@users.noreply.github.com>
2020-02-22 14:50:32 -0500

    Update EventLibrary.java

--- Intuo/app/src/main/java/com/example/intuo/EventLibrary.java
+++ Intuo/app/src/main/java/com/example/intuo/EventLibrary.java
@@ -1,1 +1,16 @@
 
+
+package com.example.intuo;
+
+public class EventLibrary {
+
+  private EventtList listE
+  
+  public void addEvent(Event e){
+    
+  }
+  public void print(){
+    System.out.println("Event Library: ")
+      
+  }
+}
