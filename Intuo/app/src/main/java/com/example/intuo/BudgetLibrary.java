d4dad0378b0863f3ed2ec4efa462fc3dd800f1f0
wengehuihuang <60371399+wengehuihuang@users.noreply.github.com>
2020-02-22 15:20:23 -0500

    Update BudgetLibrary.java

--- Intuo/app/src/main/java/com/example/intuo/BudgetLibrary.java
+++ Intuo/app/src/main/java/com/example/intuo/BudgetLibrary.java
@@ -5,8 +5,8 @@ public class BudgetLibrary {
 
   private BudgetList listB
   
-  public void addBudget(Budget e){
-    listB.insert(e)
+  public void addBudget(Budget b){
+    listB.insert(b)
   }
   public void print(){
     System.out.println("Budget Library: ")
